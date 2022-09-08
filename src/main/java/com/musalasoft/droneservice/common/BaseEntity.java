0604b9be4e64bea2b9e462a386cf36f0adad46ed
Alex Maina <maina.alex@eclectics.io>
2022-09-08 16:52:23 +0300

    Modified class diagram to include createdOn Property

---
+++ src/main/java/com/musalasoft/droneservice/common/BaseEntity.java
@@ -0,0 +1,6 @@
+package com.musalasoft.droneservice.common;/**
+* @author Alex Maina
+* @created 08/09/2022
+**/
+public class BaseEntity {
+}
---
+++ src/main/java/com/musalasoft/droneservice/model/Drone.java
@@ -0,0 +1,6 @@
+package com.musalasoft.droneservice.model;/**
+* @author Alex Maina
+* @created 08/09/2022
+**/
+public class Drone {
+}
